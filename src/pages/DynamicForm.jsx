53b92c0a2502faa410e59a7a769eb413af751a60
Mark Grech <mark.grech@fintama.com>
2023-02-07 18:55:42 +0100

    remove submit button on submit and disable form

--- src/pages/DynamicForm.jsx
+++ src/pages/DynamicForm.jsx
@@ -3,47 +3,56 @@ import "../App.css";
 import validator from "@rjsf/validator-ajv8";
 import Form from "@rjsf/mui";
 import axios from "axios";
-import { Typography } from "@mui/material";
+import { Divider, Typography } from "@mui/material";
 
 function DynamicForm() {
-
   //https://github.com/rjsf-team/react-jsonschema-form/issues/3258
 
   const [schema, setSchema] = useState(undefined);
   const [processId, setProcessId] = useState(undefined);
   const [status, setStatus] = useState(undefined);
-  const serviceId = window.location.pathname.split("/")[2]; 
+  const serviceId = window.location.pathname.split("/")[2];
 
   const fetchData = async () => {
-    
-    //get the service id from the url 
-    const productConfigRes = await axios.post("http://localhost:8080/product-configuration/" + serviceId, {});
-    
+    //get the service id from the url
+    const productConfigRes = await axios.post(
+      "http://localhost:8080/product-configuration/" + serviceId,
+      {}
+    );
+
     const processId = productConfigRes.data;
-    
+
     //call get agreements with processid
-    const res = await axios.get("http://localhost:8080/product-configuration/" + processId + "/task");
+    const res = await axios.get(
+      "http://localhost:8080/product-configuration/" + processId + "/task"
+    );
     setSchema(res.data);
 
     setProcessId(processId);
-    
+
     //call /product-configuration with processId
-    const result = await axios.get("http://localhost:8080/product-configuration/" + processId);
+    const result = await axios.get(
+      "http://localhost:8080/product-configuration/" + processId
+    );
     setStatus(result.data.status);
-    console.log(result)
-  }
+    console.log(result);
+  };
 
   const onSubmit = async (e) => {
-    if (status !== "READY_FOR_CONFIGURATION")
-      return;
+    if (status !== "READY_FOR_CONFIGURATION") return;
 
-  console.log("🚀 ~ file: DynamicForm.jsx:40 ~ onSubmit ~ e", e)
+    console.log("🚀 ~ file: DynamicForm.jsx:40 ~ onSubmit ~ e", e);
     const formData = e.formData;
-    await axios.post("http://localhost:8080/product-configuration/" + processId + "/configure", formData);
+    await axios.post(
+      "http://localhost:8080/product-configuration/" + processId + "/configure",
+      formData
+    );
 
-    const res = await axios.get("http://localhost:8080/product-configuration/" + processId);
+    const res = await axios.get(
+      "http://localhost:8080/product-configuration/" + processId
+    );
     setStatus(res.data.status);
-  }
+  };
 
   useEffect(() => {
     fetchData();
@@ -51,9 +60,16 @@ function DynamicForm() {
 
   return (
     <div className="App">
-      <Typography align="center" variant="h2">Product Configuration</Typography>
-      <Typography align="center" variant="h3">{status}</Typography>
-      {schema? <div className="card">
+      <Typography align="center" variant="h2">
+        Product Configuration
+      </Typography>
+      <Divider className="divider"/>
+      <Typography align="center" variant="h4">
+        Service Agreement Status: {status}
+      </Typography>
+      <Divider className="divider" variant="inset" component="h1" />
+      {schema ? (
+        <div className="card">
           <Form
             schema={schema}
             onSubmit={(e) => onSubmit(e)}
@@ -61,8 +77,10 @@ function DynamicForm() {
             onError={(e) => console.log("error: ", e)}
             validator={validator}
             disabled={status !== "READY_FOR_CONFIGURATION"}
+            children={status !== "READY_FOR_CONFIGURATION" ? true : false}
           />
-      </div> : undefined}
+        </div>
+      ) : undefined}
     </div>
   );
 }
